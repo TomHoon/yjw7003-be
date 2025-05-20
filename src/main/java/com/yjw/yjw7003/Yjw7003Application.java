af42a98cb3bb983af680c33aff246f5804283d93
tomhoon <gnsdl9108@naver.com>
2025-05-20 21:50:36 +0900

    first commit

---
+++ src/main/java/com/yjw/yjw7003/Yjw7003Application.java
@@ -0,0 +1,13 @@
+package com.yjw.yjw7003;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yjw7003Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yjw7003Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/yjw/yjw7003/Yjw7003ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjw.yjw7003;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yjw7003ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
