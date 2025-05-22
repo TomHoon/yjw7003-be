277516c4f7ac27de2866290f3f0f6d20d0578f63
tomhoon <gnsdl9108@naver.com>
2025-05-22 18:24:31 +0900

    feat: 로그인 유지 임시 쿠키처리

--- src/main/java/com/yjw/yjw7003/config/WebConfig.java
+++ src/main/java/com/yjw/yjw7003/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.yjw.yjw7003.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +13,23 @@ public class WebConfig implements WebMvcConfigurer {
     registry
         .addResourceHandler("/uploads/**")
         .addResourceLocations("file:uploads");
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    String[] allowedOrigins = {
+        "http://localhost:5500",
+        "http://localhost:3000",
+        "http://127.0.0.1:5500",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    };
+
+    registry.addMapping("/**")
+        .allowedOrigins(allowedOrigins) // 허용할 출처
+        .allowedMethods("GET", "POST") // 허용할 HTTP method
+        .allowCredentials(true) // 쿠키 인증 요청 허용
+        .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
   }
 
 }
--- src/main/java/com/yjw/yjw7003/controller/MemberController.java
+++ src/main/java/com/yjw/yjw7003/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.yjw.yjw7003.controller;
 
+import org.springframework.http.ResponseCookie;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.yjw.yjw7003.dto.MemberDTO;
 import com.yjw.yjw7003.service.MemberService;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -18,8 +20,31 @@ public class MemberController {
   private final MemberService memberService;
 
   @PostMapping("/login")
-  public Boolean 회원로그인(@RequestBody MemberDTO dto) {
-    return memberService.회원로그인(dto);
+  public Boolean 회원로그인(@RequestBody MemberDTO dto, HttpServletResponse response) {
+    Boolean isLogged = memberService.회원로그인(dto);
+
+    if (!isLogged)
+      return false;
+
+    else {
+      int maxAge = 60 * 60;
+
+      ResponseCookie cookie = ResponseCookie
+          .from("token-test", "1234444")
+          .domain("localhost")
+          .maxAge(maxAge)
+          .sameSite("None")
+          .secure(true)
+          .httpOnly(true)
+          .path("/")
+          .build();
+
+      response.addHeader("Set-Cookie", cookie.toString());
+
+      return isLogged;
+
+    }
+
   }
 
 }
